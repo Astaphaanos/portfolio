50bf6460d6b646b8d2b1e45d962348bf437a6465
Astaphaanos <Projeto Finalizado>
2024-11-13 13:02:34 -0300

    Menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,39 @@
 // Colocar o menu hamburguer e a imagem
 
 import './Header.css';
+import React, { useState } from 'react';
 
 const Header = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    }
+
+
     return (
         <header className="header">
-            <img src="/Images/logo.png" alt="Logo"/>
-            <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#sobre">Sobre</a></li>
-                <li><a href="#projetos">Projetos</a></li>
-                <li><a href="#contatos">Contato</a></li>
-            </ul>
+
+
+            <div className='header-logo'>
+                <img src="/Images/logo.png" alt="Logo"/>
+            </div>
+
+            <div>
+                <button className="hamburger" onClick={toggleMenu}>
+                    <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/menu.png" alt="menu"/>
+                </button>
+            </div>
+
+            <nav className={`menu ${isOpen ? 'open' : ''}`}>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#sobre">Sobre</a></li>
+                    <li><a href="#projetos">Projetos</a></li>
+                    <li><a href="#contatos">Contato</a></li>
+                </ul>
+            </nav>
         </header>
     );
 }
